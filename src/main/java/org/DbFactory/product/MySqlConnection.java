f9eeb4fae63cff0800c28842ef925c92c89c6dde
Asignment2OOP <254298@astanait.edu.kz>
2026-09-19 18:40:13 +0500

    Implement MySqlConnection as concrete product

---
+++ src/main/java/org/DbFactory/product/MySqlConnection.java
@@ -0,0 +1,21 @@
+package org.DbFactory.product;
+
+public class MySqlConnection implements Connection {
+
+    private boolean open = false;
+
+    @Override
+    public void open(){
+        open = true;
+        System.out.println("[MySQL] Connection openned using JBDC driver (com.mysql.cj.jdbc.Driver).");
+    }
+
+    @Override
+    public void close(){
+        open = false;
+        System.out.println("[My SQL] Connection closed");
+    }
+
+    @Override
+    public String describe(){return "MySQL connection (open="+ open + ")";}
+}
