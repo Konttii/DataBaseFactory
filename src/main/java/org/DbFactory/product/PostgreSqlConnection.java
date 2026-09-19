9cf8a4f6cc631a7d54af9a88db046df39c8c8397
Asignment2OOP <254298@astanait.edu.kz>
2026-09-19 18:58:21 +0500

    Implement PostgreSqlConnection as concrete product

---
+++ src/main/java/org/DbFactory/product/PostgreSqlConnection.java
@@ -0,0 +1,21 @@
+package org.DbFactory.product;
+
+public class PostgreSqlConnection implements Connection{
+
+    private boolean open = false;
+
+    @Override
+    public void open() {
+        open = true;
+        System.out.println("[PostgreSQL] Connection opened using JDBC driver (org.postgresql.Driver).");
+    }
+
+    @Override
+    public void close() {
+        open = false;
+        System.out.println("[PostgreSQL] Connection closed.");
+    }
+    @Override
+    public String describe() {return "PostgreSQL connection (open=" + open + ")";}
+
+}
