0f350f81dfc16be3720cb37a73f1c8715af8c8dc
Asignment2OOP <254298@astanait.edu.kz>
2026-09-19 20:37:29 +0500

    Integrate Factory Method and Abstract Factory demos in Main

--- src/main/java/org/dbfactory/Main.java
+++ src/main/java/org/dbfactory/Main.java
@@ -1,8 +1,35 @@
 package org.dbfactory;
 
+import org.dbfactory.client.DatabaseClient;
+import org.dbfactory.config.FactoryRegistry;
+import org.dbfactory.creator.ConnectionFactory;
+import org.dbfactory.creator.MySqlConnectionFactory;
+import org.dbfactory.creator.PostgreSqlConnectionFactory;
+import org.dbfactory.family.DatabaseFamilyFactory;
 
 public class Main {
-    static void main() {
 
+    public static void main(String[] args) {
+        System.out.println("=== Part A: Factory Method ===");
+        demoFactoryMethod();
+
+        System.out.println();
+        System.out.println("=== Part B: Abstract Factory ===");
+        demoAbstractFactory("mysql");
+        demoAbstractFactory("postgres");
+    }
+
+    private static void demoFactoryMethod() {
+        ConnectionFactory mysqlFactory = new MySqlConnectionFactory();
+        ConnectionFactory postgresFactory = new PostgreSqlConnectionFactory();
+
+        System.out.println(mysqlFactory.connectAndDescribe());
+        System.out.println(postgresFactory.connectAndDescribe());
+    }
+
+    private static void demoAbstractFactory(String databaseType) {
+        DatabaseFamilyFactory factory = FactoryRegistry.get(databaseType);
+        DatabaseClient client = new DatabaseClient(factory);
+        client.runSampleWorkflow("SELECT * FROM users WHERE active = true");
     }
 }
